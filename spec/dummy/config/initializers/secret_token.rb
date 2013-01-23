e7885722200b407cc61cf8c53f9b7097dfcc4807
barsukov <pro.barsukov@gmail.com>
2013-01-23 18:38:45 +0400

    Add dummy app to rspec

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '38a17e57f4512a305417dab11e12333b33f7005411cd8537432b9dad434dc8e3b955c3e38e8bc62b5c63c0a67acb4ef926d79f85b87f9d20847f49ea35812d37'
